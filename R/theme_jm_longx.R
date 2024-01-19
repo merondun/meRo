c1ea8973f94f9318c2e5c93b319706c179972ccd
merondun <heritabilities@gmail.com>
2024-01-19 02:23:51 +0100

    update man

--- R/theme_jm_longx.R
+++ R/theme_jm_longx.R
@@ -1,15 +1,4 @@
 #' Custom ggplot2 Theme Function
-#'
-#' This function applies a custom ggplot2 theme with specific aesthetic modifications. It modifies the base black-and-white theme by adjusting the text elements including axis text, plot title, legend text, and legend title, and sets their font family to "Myriad". Additionally, it allows the user to specify the font size for these elements. The function also positions the legend at the top of the plot.
-#' 
-#' @param font_size Numeric value specifying the font size to be used for text elements in the plot. The default value is 10.
-#' 
-#' @return A ggplot2 theme object that can be added to ggplot2 plot objects.
-#' 
-#' @export
-#' 
-#' @import ggplot2
-
 .onLoad <- function(libname, pkgname) {
   tryCatch({
     requireNamespace("sysfonts", quietly = TRUE)
@@ -23,6 +12,17 @@
     packageStartupMessage(message)
   })
 }
+
+#'
+#' This function applies a custom ggplot2 theme with specific aesthetic modifications. It modifies the base black-and-white theme by adjusting the text elements including axis text, plot title, legend text, and legend title, and sets their font family to "Myriad". Additionally, it allows the user to specify the font size for these elements. The function also positions the legend at the top of the plot.
+#' 
+#' @param font_size Numeric value specifying the font size to be used for text elements in the plot. The default value is 10.
+#' 
+#' @return A ggplot2 theme object that can be added to ggplot2 plot objects.
+#' 
+#' @export
+#' 
+#' @import ggplot2
 
 theme_jm_longx <- function(font_size) {
   ggplot2::theme_bw() +
